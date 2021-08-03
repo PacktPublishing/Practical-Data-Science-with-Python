4cb799089565a0b5907dedf784a9eb33d277e79b
Nate George <wordsforthewise@gmail.com>
2021-08-03 17:16:07 -0600

    update ch18 and 19

--- Chapter19/default_dashboard.py
+++ Chapter19/default_dashboard.py
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix
 
 st.set_page_config(layout="wide")
 
-df = pd.read_excel('data/default of credit card clients.xls', skiprows=1, index_col='ID')
+df = pd.read_excel('data/default of credit card clients.xls', skiprows=1, index_col='ID').sample(1000)
 
 phik_corr = df.phik_matrix()
 correlogram = sns.heatmap(phik_corr)
@@ -20,20 +20,22 @@ barchart = px.histogram(df, x='PAY_0', color='default payment next month', barmo
 col1, col2 = st.beta_columns(2)
 col1.write(correlogram.figure)
 col2.write(barchart)
+plt.clf()
 
 setup = pyclf.setup(df, target='default payment next month', silent=True)
 lgbm = pyclf.create_model('lightgbm')
 lgbm, tuner = pyclf.tune_model(lgbm, return_tuner=True)
 
+
 feature_importances = pd.Series(lgbm.feature_importances_, index=pyclf.get_config('X').columns)
 feat_imp_plot = feature_importances.nlargest(20).plot(kind='barh')
 feat_imp_plot.invert_yaxis()
 
-predictions = pyclf.predict_model(lgbm, df)
-cm = plot_confusion_matrix(confusion_matrix(df['default payment next month'], predictions['Label']))
-
 st.title(f"CV Accuracy is {round(tuner.cv_results_['mean_test_score'].mean(), 3)}")
-plt.clf()
 col1, col2 = st.beta_columns(2)
 col1.write(feat_imp_plot.figure)
+
+predictions = pyclf.predict_model(lgbm, df)
+plt.clf()
+cm = plot_confusion_matrix(confusion_matrix(df['default payment next month'], predictions['Label']))
 col2.write(cm[1].figure)
